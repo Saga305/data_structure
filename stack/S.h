a6806d0aa0c9d43ad99fb36216e8a1a99468b408
ajaypatel305 <ajay.patel305@gmail.com>
2017-09-05 14:02:08 +0530

    Indentation

--- stack/S.h
+++ stack/S.h
@@ -6,28 +6,21 @@ using namespace std;
 
 class Stack
 {
-private:
-int _top;
-int _capacity;
-int *_arr;
-public:
-Stack() {}
-Stack(int);
-int top();
-void top(int);
-int capacity();
-bool isStackEmpty();
-bool isStackFull();
-void fill(int,int);
-int unFill(int);
-void print();
-void push(int);
-int pop();
-
-
-
-
-
-
-
+	private:
+		int _top;
+		int _capacity;
+		int *_arr;
+	public:
+		Stack() {}
+		Stack(int);
+		int top();
+		void top(int);
+		int capacity();
+		bool isStackEmpty();
+		bool isStackFull();
+		void fill(int,int);
+		int unFill(int);
+		void print();
+		void push(int);
+		int pop();
 };
